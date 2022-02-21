826073f6f9c472f2569841aeacf8d353b9439729
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-02-21 21:39:58 +0900

    useEffects, cleanup Function

--- Make_MovieAppService_with_ReactJs/React_Basic/6.Create_React_APP/react-for-beginners/src/App.js
+++ Make_MovieAppService_with_ReactJs/React_Basic/6.Create_React_APP/react-for-beginners/src/App.js
@@ -1,11 +1,52 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("I run all the time");
+  // Component가 처음 rendering 될 때 실행되고 다시는 실행되지 않을 function을 넣음.
+  // const iRunOnlyOnce = () => {
+  //   console.log("I run only once");
+  // };
+  // useEffect(iRunOnlyOnce, []);
+
+  // useEffect는 코드를 딱 한번만 실행될 수 있도록 보호해준다.
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log(" I run when 'keyword' changes");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [counter, keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <div>
+        <h1 className={styles.title}>Welcome Back!</h1>
+        <Button text={"Continue"} />
+      </div>
+      <div>
+        <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      </div>
+      <div>
+        <h1>{counter}</h1>
+        <button onClick={onClick}>click me!</button>
+      </div>
     </div>
   );
 }
--- Make_MovieAppService_with_ReactJs/React_Basic/6.Create_React_APP/react-for-beginners/src/index.js
+++ Make_MovieAppService_with_ReactJs/React_Basic/6.Create_React_APP/react-for-beginners/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// import App_Cleanup_Function from "./App_Cleanup_Function";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +9,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App_Cleanup_Function />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
